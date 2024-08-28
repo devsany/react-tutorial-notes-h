f575df5123c25c1e2d0b984fbab884878d407be0
devsany <sunnyrajbodhgaya19@gmail.com>
2024-08-28 21:10:13 +0530

    done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Hookcounter from "./useState/Hookcounter";
 import UseStateObject from "./useStateObject/UseStateObject";
+import UseStateArray from "./useStateArray/UseStateArray";
 
 function App() {
   return (
     <div className="App">
-      <UseStateObject />
+      {/* <Hookcounter/> */}
+      {/* <UseStateObject /> */}
+      <UseStateArray />
     </div>
   );
 }
